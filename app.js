d1c1a7d62359035f758f7570ea76b8241af86129
Abdul Azeem <abdulazeem@devnexus.in>
2025-08-28 11:42:10 +0530

    separate Routes and controller

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import cors from 'cors';
 import webhookRoutes from './Routes/webhookRoutes.js'
-import callback from './Routes/CallbackRoute.js';
+import callback from './Routes/CallbackRoute1.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
